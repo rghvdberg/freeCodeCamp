75f72e5fcdae44117360760be16cedcec191de2a
Rob van den Berg <rghvdberg@gmail.com>
2021-09-06 21:05:08 +0200

    regex solutions 01...10

---
+++ javascript-algorithms-and-data-structures/regular-expressions/04-ignore-case-while-matching.js
@@ -0,0 +1,6 @@
+
+let myString = "freeCodeCamp";
+let fccRegex = /freecodecamp/i; // Change this line
+let result = fccRegex.test(myString);
+
+console.log(result);
---
+++ javascript-algorithms-and-data-structures/regular-expressions/09-match-letters-of-the-alphabet.js
@@ -0,0 +1,6 @@
+let quoteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/gi; // Change this line
+let result = quoteSample.match(alphabetRegex); // Change this line
+
+console.log(result,result.length);
+
